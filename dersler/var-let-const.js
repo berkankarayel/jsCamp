532efdb6367ea13ebbaa9a185d9229230627c5c5
berkankarayel <berkankarayel.147@gmail.com>
2024-03-11 23:56:42 +0300

    gün1

--- dersler/var-let-const.js
+++ dersler/var-let-const.js
@@ -1,127 +1,50 @@
-// function scope sadece foknsiyon içinde tanımlanır.
-// global scope ise her yerden tanımlanabilir
-var degiskenismi = 10;
-let deger = 20;
+// Var - let - const
+// es6 ile beraber let ve const geldi. önceden 'var' vardı.
+/*
 
-if (true) {
-  console.log(degiskenismi);
-  console.log(deger);
-}
+--> global scope : heryerden erişelebilen scopedur.ram bellekte fazladan yer kaplar 
+--> function scope : sadece tanımladığımız yerde kullanabilriz.
+--> block scope : sadece bloğun içinde çalışabilen scopedur.
 
-function method1() {
-  console.log(degiskenismi);
-}
-method1();
 
-// var : function scope. varla tanımladiğimiz herşey function scope olur . ram bellekte çok fazla yer kaplar.var iki kere tanımlanabilir sondakini referans alır .
-// let : block scope özelliğine sahiptir.let iki kere tanımlanmaz.
-// const :block scope özeliğine sahipir.
+*/
+// var: Herşey function scopedur. yani blok içinde tanımlayıp
+// functuon da erişebilriz.
+// ram bellekte çok yer kaplar dejavantajı budur.
 
-function selamVer() {
-  var selam = "herkese selam"; // function scope
+function Selamver() {
+  var selam = "Herkese selam ";
 
-  if (true) {
-    var b = 10;
-    let c = 35;
-  }
-  // console.log(c);  burdan erişelemez çünkü let block scope dur.
-  console.log(b);
   console.log(selam);
 }
-selamVer();
 
-/*
-let a = 5;   bu çalışmaz 
-let a =10;
+Selamver();
 
-var a =5;
-var a =10; bu çalışır.
+//! let/const : blok scope özlelliği vardır.
 
-*/
+var c = 5;
+var c = 10;
 
-// Const ve let arasındaki fark :
+console.log(c);
 
-/*
-  const sabit değişmez 
-  let değiştirelebilir 
+//!  let ile aynı değişkenden birdaha oluşmaz
+//let b = 5;
+//let b = 5;
 
-  örnek :
+//? let ve const farkı
+// cons ile bir değşişkenin değeri tekrara güncellenmez değiştirelemz
 
+// const x = 16;
+// x = 21;
+// console.log(x);
 
-  const a =10;
-  a = 5;
-  console.log(a); hata verir
-
-
-  let a =15;
-  a=22;
-  console.log(a);çalışır
-
-*/
+let e = 21;
+e = 22;
+console.log(e);
 
 const user = {
   username: "berkan",
-  password: "123",
+  passqord: "123",
 };
 
-user.username = "berkan karayel"; // bu şekilde değiştirebiliriz
 console.log(user);
-
-/*
- ! DİYALOG KUTULARi
-  
- ALERT : uyarı 
- PROMPT : kullancııdan değer al 
- CONFİRM : true veya false 
-
- bunlar window objesi içinde yer alır .
-
- */
-
-console.log(window);
-alert("lütfen isminizi girniz");
-
-// kullanıcdan değer alma
-
-let isim = prompt("isminizi girniz"); // kullancıı ekrana isim girer.
-console.log(isim);
-
-// kullanıcıdan alınan değerlerinin tipi her zaman stringdir.
-console.log(typeof isim);
-console.log(typeof yas);
-
-let yas = prompt("yaşınızı giriniz");
-
-confirm("silmek istedğinize eminmisiniz"); // tamama basarsak true diğeri false olarak yazdırır consolda .
-
-let a = 5;
-let b = Number("10");
-
-// strinden -->numbera dönüşüm
-
-let c = Number(b);
-console.log(a + b);
-console.log(typeof c);
-
-let e = parseInt("10");
-let t = parseFloat("12.5");
-// number --> string
-
-let x = String(55);
-console.log(typeof x);
-console.log(x);
-// deneme
-
-console.log("deneme yazılım");
-let d = Number(b);
-console.log(a + b);
-console.log(typeof c);
-
-let f = parseInt("10");
-let g = parseFloat("12.5");
-// number --> string
-
-let h = String(55);
-console.log(typeof x);
-console.log(x);
-// deneme
---
+++ dersler/veritipleri.js
@@ -0,0 +1,36 @@
+//? Veri tipleri -->
+//1-) String
+
+let isim = "Enes";
+console.log(typeof isim);
+
+//2-) Number
+
+let sayi = 10;
+console.log(sayi);
+
+// 3-) Boolean : true veya false
+
+console.log(5 > 2);
+
+let a = 3;
+let b = 12;
+let sonuc = a + b;
+console.log(sonuc > 21);
+
+//4-) Null : boş
+
+let c = null;
+
+//5-)undefined: tanımlanmamış
+
+let z;
+console.log(typeof z);
+
+//6-) Object
+let insan = {
+  // object veri tipi oldu
+  isim: "berkan",
+  soyisim: "karayel",
+  yas: 23,
+};
