9d5b811dcff53df3952c8b744e408d16b2d842f2
berkankarayel <berkankarayel.147@gmail.com>
2024-03-26 17:39:15 +0300

    gün4

--- dersler/metotlar.js
+++ dersler/metotlar.js
@@ -13,25 +13,35 @@
 
 //! Parametreli metot tanımlama
 
-function yazdir(isim, soyisim) {
-  console.log(isim + soyisim);
-}
+// function yazdir(isim, soyisim) {
+//   console.log(isim + soyisim);
+// }
 
-yazdir("berkan", "karayel");
+// yazdir("berkan", "karayel");
+
+// function cube(sayi) {
+//   console.log(Math.pow(sayi, 3)); // math.pow fonksiyonu küp alır.
+// }
+// cube(5);
+
+// let yas = Number(prompt("Lütfen yasınızı giriniz"));
+
+// function kontrolEt(berkan) {
+//   if (berkan >= 18) {
+//     console.log("ehliyet alabilir");
+//   } else {
+//     console.log("ehliyet alamaz");
+//   }
+// }
+
+// kontrolEt(yas);
+
+//! Geriye Değer Döndüren metot tanımlama
 
 function cube(sayi) {
-  console.log(Math.pow(sayi, 3)); // math.pow fonksiyonu küp alır.
+  let sonuc = sayi * sayi * sayi;
+  return sonuc;
+  // returdan sonra kodlar erişilmez
 }
-cube(5);
-
-let yas = Number(prompt("Lütfen yasınızı giriniz"));
-
-function kontrolEt(yas) {
-  if (yas >= 18) {
-    console.log("ehliyet alabilir");
-  } else {
-    console.log("ehliyet alamaz");
-  }
-}
-
-kontrolEt(yas21);
+let donenDeger = cube(1);
+console.log(donenDeger);
---
+++ dersler/metotlar.ornek1.js
@@ -0,0 +1,17 @@
+//! Bir cümledeki harf sayısnı bulma uygulaması
+
+let metin = "Şuanda Yalova 'da Javascript eğitimi çekmekteyim.";
+
+let harf = prompt("harfi giriniz");
+
+let sonuc = bul(harf);
+console.log("harf sayisi:a" + sonuc);
+function bul(harfsayisi) {
+  let toplam = 0;
+  for (let i = 0; i < metin.length; i++) {
+    if (metin.charAt(i).toLowerCase() == harf.toLowerCase()) {
+      toplam++;
+    }
+  }
+  return toplam;
+}
