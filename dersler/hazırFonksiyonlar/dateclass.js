759bf134f3a7e45e4a2271eb30b577d5caa256de
berkankarayel <berkankarayel.147@gmail.com>
2024-05-03 00:57:18 +0300

    functionsdate

--- dersler/hazırFonksiyonlar/dateclass.js
+++ dersler/hazırFonksiyonlar/dateclass.js
@@ -3,29 +3,29 @@
 // let tarih = new Date(1999,08,07,10,40,50);
 // console.log(typeof tarih);
 
-// let tarih = new Date();
+let tarih = new Date(); // şuanki tarihi adında obje oluşturduk
 // console.log(tarih.toString());
 // console.log(typeof tarih.toString());
 // console.log(tarih);
 
-// console.log(tarih.getFullYear());
-// console.log(tarih.getDate());
-// console.log(tarih.getDay());
-// console.log(tarih.getHours());
-// console.log(tarih.getMilliseconds());
-// console.log(tarih.getMinutes());
-// console.log(tarih.getMonth()+1);
-// console.log(tarih.getSeconds());
-// console.log(tarih.toLocaleDateString());
-// console.log(tarih.toLocaleTimeString());
-// console.log(tarih.toLocaleString());
+console.log(tarih.getFullYear()); // get metotoları tarihin içinden değer alır. yıl alır
+console.log(tarih.getDate()); // ayın kaçıncı günü olduğunu veirir.
+console.log(tarih.getDay()); // haftanın gününü alır sayı olarak verir.
+console.log(tarih.getHours()); // saatin kaç olduğunu söylüyor
+console.log(tarih.getMilliseconds());
+console.log(tarih.getMinutes());
+console.log(tarih.getMonth() + 1); // aylar  0 dan başlıyor o yüzden attı 1 yaparız
+console.log(tarih.getSeconds());
+console.log(tarih.toLocaleDateString()); // tarıhi gün ay yıl olarak verir
+console.log(tarih.toLocaleTimeString()); // saat gün dakika veririyor
+console.log(tarih.toLocaleString()); // iksiinide birleşimi
 
 //SET METOTLARI
-// console.log(tarih);
-// tarih.setHours(15);
-// tarih.setDate(24);
-// tarih.setMonth(11);
-// tarih.setMinutes(50);
+console.log(tarih);
+tarih.setHours(15); // tarıhin saatini değişitrdik
+tarih.setDate(24); // tarihin günü dewğişştirk
+tarih.setMonth(11); // tarihn ayını değiştiridk
+tarih.setMinutes(50); // tarihin dakikasını değiştridik
 
-// tarih.setHours(tarih.getHours()+2);
-// console.log(tarih);
+tarih.setHours(tarih.getHours() + 2);
+console.log(tarih);
