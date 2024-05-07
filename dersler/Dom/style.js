bbfa74fa4a33db012735e8d73c55654dfcd7c022
berkankarayel <berkankarayel.147@gmail.com>
2024-05-07 15:57:26 +0300

    dom

--- dersler/Dom/style.js
+++ dersler/Dom/style.js
@@ -1,10 +1,21 @@
-// style özelliği
+const todo = document.querySelectorAll(".list-group-item")[0];
+const todoList = document.querySelector(".list-group");
 
-const todo = document.querySelector(".card-body");
-console.log(todo);
+const clearButton = document.querySelector("#todoClearButton");
 
-todo.style.color = "blue";
-todo.style.backgroundColor = "brown";
-todo.style.fontWeight = "bold";
-todo.style.paddingTop = "20px";
-todo.style.paddingLeft = "70px";
+console.log(clearButton);
+
+todo.style.color = "white"; // yazının rengi
+todo.style.backgroundColor = "blue"; // arkalan rengi
+todo.style.fontWeight = "bold"; //yazının kalınlığı
+todo.style.paddingTop = "20px"; // üsten boşluk
+todo.style.paddingLeft = "50px";
+
+todoList.style.marginTop = "60px";
+// todoList.style.marginLeft = "100px";
+
+clearButton.style.backgroundColor = "red";
+clearButton.style.fontWeight = "bold";
+clearButton.style.padding = "10px";
+// clearButton.style.marginTop="100px";
+clearButton.style.borderRadius = "10px";
