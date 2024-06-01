a1d1d338d75101899f42dc51bd9d009faa6658f6
berkankarayel <berkankarayel.147@gmail.com>
2024-06-01 18:22:37 +0300

    ES6

---
+++ dersler/ES6/arrowfunction.js
@@ -0,0 +1,20 @@
+// function yazdir() {
+//   console.log("merhaba");
+// }
+// yazdir();
+
+//! yukarıdan yazının arrow function ile yazımı
+
+// const yazdir = () => {
+//   console.log("merhaba");
+// };
+// yazdir();
+
+//! parametreli arrow function
+
+const yazdir = (firstName, lastName, yasi) => {
+  console.log("merhaba", firstName, lastName, yasi);
+  let a = 5;
+  console.log(a);
+};
+yazdir("berkan", "karayel", 21);
